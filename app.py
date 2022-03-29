b9fb505c28183e5f96e8f2bc71eae1cb54809d5f
mag574 <mag574@txstate.edu>
2022-03-28 22:28:36 -0500

    Routing Updates

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from flask import Flask, render_template, request
-from flask_bootstrap5 import Bootstrap
-from werkzeug.utils import secure_filename
+from flask_bootstrap import Bootstrap5
+from werkzeug.utils import secure_filename, redirect
 import os
 from os import abort
 import PyPDF2
 
 app = Flask(__name__)
-bootstrap = Bootstrap(app)
+bootstrap = Bootstrap5(app)
 
 user = {}
 
@@ -14,8 +14,13 @@ app.config['MAX_CONTENT_LENGTH'] = 1024*1024
 app.config['UPLOAD_EXTENSIONS'] = ['.pdf']
 app.config['UPLOAD_PATH'] = 'uploads'
 
+@app.route('/home')
 @app.route('/')
-def index():  # put application's code here
+def index():
+    return render_template('home.html')
+
+@app.route('/application')
+def parse():  # put application's code here
     pdfFileObj = open('static/sample_resume.pdf', 'rb')
     # Creating a pdf reader object
     pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
@@ -30,10 +35,10 @@ def index():  # put application's code here
         # Extracting text from page
         # And splitting it into chunks of lines
         text = pageObj.extractText().split('\n')
-        print(text)
+        print(pageObj.extractText())
         # Finally the lines are stored into list
         # For iterating over list a loop is used
-        user['name'] = text[40]
+        user['name'] = text[10]
         user['email'] = text[47]
         user['education'] = text[25]
         #user['major']
@@ -48,7 +53,7 @@ def index():  # put application's code here
         '''
     # closing the pdf file object
     pdfFileObj.close()
-    return render_template("index.html", user=user)
+    return render_template("application.html", user=user)
 
 @app.route('/upload', methods=['GET', 'POST'])
 def upload():
@@ -60,7 +65,8 @@ def upload():
             if file_ext not in app.config['UPLOAD_EXTENSIONS']:
                 abort(400)
             uploaded_file.save(os.path.join(app.config['UPLOAD_PATH'], filename))
-    return render_template('fileUpload.html')
+            return redirect('/application')
+    return render_template('upload.html')
 
 if __name__ == '__main__':
     app.run()
